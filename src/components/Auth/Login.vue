8ca1f5f63a0376fefab54ca3741581291e784f1b
124c4a11 <124c4a11@mail.ru>
2019-01-01 00:45:29 +0300

    Login

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,8 +1,43 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Login</h1>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md5>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="light-blue">
+            <v-toolbar-title>Login form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" validation>
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                prepend-icon="person"
+                name="email"
+                label="E-mail"
+                type="email"
+                color="light-blue"
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+                color="light-blue"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              @click="onSubmit"
+              :disabled="!valid"
+              color="light-blue"
+              class="white--text"
+            >Login</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -15,7 +50,32 @@ export default {
 
   data () {
     return {
+      valid: false,
 
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be at least 6 characters'
+      ],
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      if (this.$refs.form.validate()) {
+        const user = {
+          emali: this.email,
+          password: this.password
+        };
+
+        console.log(user);
+      }
     }
   }
 }
