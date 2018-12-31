39cfb0728cfa0708834b81ab74ef79117b8e6dcc
124c4a11 <124c4a11@mail.ru>
2018-12-31 16:05:06 +0300

    navigation markup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,48 @@
 <template>
   <v-app>
-    <v-navigation-drawer app temporary></v-navigation-drawer>
-    <v-toolbar app dark color="teal lighten-1">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-navigation-drawer
+      v-model="drawerIsOpen"
+      app
+      temporary
+    >
+      <v-list>
+        <v-list-tile
+          v-for="(link, ndx) in links"
+          :key="ndx"
+          :to="link.url"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app dark color="light-blue">
+      <v-toolbar-side-icon
+        @click="drawerIsOpen = !drawerIsOpen"
+        class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
       <v-toolbar-title>Billboard</v-toolbar-title>
       <v-spacer></v-spacer>
+
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
+        <v-btn
+          v-for="(link, ndx) in links"
+          :key="ndx"
+          :to="link.url"
+          flat
+        >
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -19,5 +53,19 @@
 
 export default {
   name: 'App',
+
+  data () {
+    return {
+      drawerIsOpen: false,
+
+      links: [
+        { title: 'Login', icon: 'lock', url: '/login' },
+        { title: 'Registration', icon: 'face', url: '/registration' },
+        { title: 'Orders', icon: 'bookmark_border', url: '/orders' },
+        { title: 'New ad', icon: 'note_add', url: '/new' },
+        { title: 'My ads', icon: 'list', url: '/list' },
+      ]
+    }
+  }
 }
 </script>
