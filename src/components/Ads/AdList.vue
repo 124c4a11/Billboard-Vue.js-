da912bb5984902398739e93758598cc58bef1c4f
124c4a11 <124c4a11@mail.ru>
2019-01-04 17:18:02 +0300

    AdList

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,36 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>AdList</h1>
+    <v-layout row justify-center>
+      <v-flex xs12 md8 lg6>
+        <h1 class="text--secondary mb-3">My ads</h1>
+        <v-card
+          v-for="ad in ads"
+          :key="ad.id"
+          class="evelation-10 mb-3"
+        >
+          <v-layout row>
+            <v-flex xs4>
+              <v-card-media
+                :src="ad.imgSrc"
+                height="175"
+              ></v-card-media>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ ad.title }}</h2>
+                <p>{{ ad.description }}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  :to="`/ad/${ad.id}`"
+                  color="light-blue"
+                  class="white--text"
+                >Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -15,7 +43,29 @@ export default {
 
   data () {
     return {
-
+      ads: [
+        {
+          id: '1',
+          title: 'Ad title',
+          description: 'Ad description',
+          imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          promo: true
+        },
+        {
+          id: '2',
+          title: 'Ad title',
+          description: 'Ad description',
+          imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          promo: true
+        },
+        {
+          id: '3',
+          title: 'Ad title',
+          description: 'Ad description',
+          imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          promo: false
+        },
+      ]
     }
   }
 }
