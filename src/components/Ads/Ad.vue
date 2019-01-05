67f8f5f0f632c102b6e34eb46d0879bdfc2de8a4
124c4a11 <124c4a11@mail.ru>
2019-01-05 18:16:25 +0300

    Ad

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,25 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Ad</h1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="2.75"
+          ></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h1 class="headline mb-3">Kangaroo Valley Safari</h1>
+              <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat color="orange">Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
