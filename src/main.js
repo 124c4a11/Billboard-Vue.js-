3018b239b6aba70b73ea1a9c3dd40b3ced5c4999
124c4a11 <124c4a11@mail.ru>
2019-01-06 17:59:21 +0300

    Firebase: initializeApp

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import * as fb from 'firebase';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+import './plugins/vuetify';
+import App from './App.vue';
+
+
+Vue.config.productionTip = false;
+
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  created () {
+    fb.initializeApp ({
+      apiKey: 'AIzaSyCLVu4_XzZEueUPlTto72lMQ5mLwjKlC9Y',
+      authDomain: 'billboard-dc964.firebaseapp.com',
+      databaseURL: 'https://billboard-dc964.firebaseio.com',
+      projectId: 'billboard-dc964',
+      storageBucket: 'billboard-dc964.appspot.com',
+      messagingSenderId: '98307806189'
+    });
+  }
+}).$mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import ads from './modules/ads';
+import user from './modules/user';
 
 
 Vue.use(Vuex);
@@ -9,6 +10,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    ads
+    ads,
+    user
   }
 });
